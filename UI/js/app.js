aaf9158d5188c4940add786df73824d0611930be
cleave3 <owhiroroeghele@gmail.com>
2019-05-13 08:44:41 +0100

    post ads page added

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,17 +1,11 @@
-/******DISPLAY BOXES *********/
+/****** VIEW CARS AND ORDER *********/
+
 const infoBox = document.querySelectorAll('.display-box');
-// const details = document.querySelector('.car-details');
-// const displayArea = document.querySelector('.display-area');
-// const back = document.querySelector('.back-button');
 
 infoBox.forEach(element => {
     element.addEventListener('click', (event) => {
         if(event.target.className == 'car-button'){
-            // displayArea.style.display ='none';
-            // details.style.display ='block';
-            // details.insertAdjacentElement("afterbegin", element);
-            // window.location ='index.html';
-            
+			confirm('CONFIRM ORDER !');
         }else {
 			window.location ='car_details.html';
 		}
