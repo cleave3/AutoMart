469ce1050f22ad0ce77c25474aa4f0f637bd2bfa
cleave3 <owhiroroeghele@gmail.com>
2019-05-29 06:28:00 +0100

    make order endpoint added

---
+++ server/dummy/models/order_model.js
@@ -0,0 +1,11 @@
+class order {
+  constructor() {
+    this.id = null;
+    this.buyer = null;
+    this.car_id = null;
+    this.created_on = null;
+    this.amount = null;
+    this.status = null;
+  }
+}
+export default order;
