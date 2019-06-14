27bad713f49cfbd6f50e8f0df6c67cb706a669e6
cleave3 <owhiroroeghele@gmail.com>
2019-06-14 07:22:18 +0100

    setup automart database

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('database connected sucessfully');
+});
+
+export default pool;
