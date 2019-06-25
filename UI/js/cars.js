1acf4fad5ee57622906afe54f7eeef28ad1e3355
cleave3 <owhiroroeghele@gmail.com>
2019-06-25 16:09:51 +0100

    Fix buy button click event listener not working on search results

--- UI/js/cars.js
+++ UI/js/cars.js
@@ -1,5 +1,7 @@
 const display = document.querySelector('.display-area');
+const displaySearch = document.querySelector('.search-result');
 const spinner = document.querySelector('.loader');
+const spinner2 = document.querySelector('.loader2');
 const carDetails = document.querySelector('.car-details');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.closeModal');
@@ -28,7 +30,7 @@ const loading = async () => {
   spinner.style.display = 'block';
 };
 
-/** DISPLAY DATA ON PAGE */
+/** TEMPLATE FOR DISPLAYING DATA ON PAGE */
 const displayCars = async (car) => {
   const template = `<div class="display-box" car-id=${car.car_id}>
   <img src=${car.image_url} alt="ford">
@@ -38,6 +40,43 @@ const displayCars = async (car) => {
   <button class="car-button">BUY</button>
   </div>`;
   display.insertAdjacentHTML('afterbegin', template);
+};
+
+/** TEMPLATE FOR DISPLAYING SEARCH RESULT ON PAGE */
+const searchCars = async (car) => {
+  displaySearch.innerHTML = '';
+  const template = `<div class="display-box" car-id=${car.car_id}>
+  <img src=${car.image_url} alt="ford">
+  <p class="car-manufacturer"><b>Manufacturer: </b>${car.manufacturer}</p>
+  <p class="car-model"><b>Model: </b>${car.model}</p>
+  <p class="car-price"><b>Price: </b><b>&#8358</b>${car.price}</p>
+  <button class="car-button">BUY</button>
+  </div>`;
+  displaySearch.insertAdjacentHTML('afterbegin', template);
+};
+
+/**
+ * VIEW SPECIFIC CAR TEMPLATE
+ * @param {object} data 
+ */
+const displayDetails = async (data) => {
+  const detailTemplate = `<div class="details-display-box" car-id=${data.car_id}>
+  <img src=${data.image_url} alt="Toyota Camry">
+  <div id="car-specs">
+  <p id="" class="car-manufacturer"><b>Manufacturer: </b>${data.manufacturer}</p>
+  <p class="car-model"><b>Model: </b>${data.model}</p>
+  <p class="car-model"><b>Body: </b>${data.body_type}</p>
+  <p class="car-transimission"><b>Transmission: </b>${data.transmission_type}
+  <p class="car-model"><b>State: </b>${data.state}</p>
+  <p class="car-price"><b>Price: </b><b>&#8358</b>${data.price}</p>
+  </div>
+  <div class="car-description-container"> 
+  <h3>DESCRIPTION</h3>
+  <p>${data.description}</p>
+  </div>
+  <button class="buy-button">BUY NOW</button>
+  <a href="./car_view.html"><button class="back-button">CONTINUE SHOPPING</button></a></div>`;
+  await carDetails.insertAdjacentHTML('afterbegin', detailTemplate);
 };
 
 /**
@@ -77,6 +116,7 @@ getData();
 const searchByPrice = async () => {
   validateFields();
   display.innerHTML = '';
+  spinner2.style.display = 'block';
   try {
     const payload = await fetch(`https://cleave-automart.herokuapp.com/api/v1/car?status=available&min_price=${minPrice.value}&max_price=${maxPrice.value}`);
     const cars = await payload.json();
@@ -85,15 +125,16 @@ const searchByPrice = async () => {
     if (noCar) {
       display.style.color = 'red';
       display.innerHTML = noCar;
+      spinner2.style.display = 'none';
       return;
     }
     await res.forEach((data) => {
       if (data) {
-        spinner.style.display = 'none';
+        spinner2.style.display = 'none';
         display.style.color = '';
-        displayCars(data);
+        searchCars(data);
       }
-      spinner.style.display = 'none';
+      spinner2.style.display = 'none';
     });
   } catch (error) {
     display.style.color = 'red';
@@ -101,40 +142,35 @@ const searchByPrice = async () => {
   }
 };
 
+/** ADD EVENT LISTENERS TO SEARCH BUTTON */
 search.addEventListener('click', (e) => {
   e.preventDefault();
   loading();
   searchByPrice();
 });
 
-display.addEventListener('click', async (e) => {
+/**
+ * VIEW A SPECIFIC CAR
+ * @param {string} id 
+ */
+const viewCarDetails = async (id) => {
+  carDetails.innerHTML = '';
+  modal.style.display = 'block';
+  const payload = await fetch(`https://cleave-automart.herokuapp.com/api/v1/car/${id}`);
+  const car = await payload.json();
+  const { data } = car;
+  displayDetails(data);
+};
+
+/** ADD EVENT LISTENER TO DOCUMENT */
+document.addEventListener('click', (e) => {
   if (e.target.className === 'car-button') {
-    carDetails.innerHTML = '';
     const id = e.target.parentElement.getAttribute('car-id');
-    const payload = await fetch(`https://cleave-automart.herokuapp.com/api/v1/car/${id}`);
-    const car = await payload.json();
-    modal.style.display = 'block';
-    const { data } = car;
-    const detailTemplate = `<div class="details-display-box" car-id=${data.car_id}>
-      <img src=${data.image_url} alt="Toyota Camry">
-      <div id="car-specs">
-      <p id="" class="car-manufacturer"><b>Manufacturer: </b>${data.manufacturer}</p>
-      <p class="car-model"><b>Model: </b>${data.model}</p>
-      <p class="car-model"><b>Body: </b>${data.body_type}</p>
-      <p class="car-transimission"><b>Transmission: </b>${data.transmission_type}
-      <p class="car-model"><b>State: </b>${data.state}</p>
-      <p class="car-price"><b>Price: </b><b>&#8358</b>${data.price}</p>
-      </div>
-      <div class="car-description-container"> 
-      <h3>DESCRIPTION</h3>
-      <p>${data.description}</p>
-      </div>
-      <button class="buy-button">BUY NOW</button>
-      <a href="./car_view.html"><button class="back-button">CONTINUE SHOPPING</button></a></div>`;
-    await carDetails.insertAdjacentHTML('afterbegin', detailTemplate);
+    viewCarDetails(id);
   }
 });
 
+/** CLOSE MODAL */
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none';
 });
