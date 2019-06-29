24724ba25840a1fb67c4cb1112075eee0e674381
cleave3 <owhiroroeghele@gmail.com>
2019-06-29 17:12:20 +0100

    Add functionality for user to update car price

--- UI/js/dasboard.js
+++ UI/js/dasboard.js
@@ -1,11 +1,13 @@
 const display = document.querySelector('#ads-container');
 const spinner = document.querySelector('.loader');
-const statusDiv = document.querySelector('.status');
-const soldBtn = document.querySelector('.sold-botton');
 const response = document.querySelector('.result');
 const token = sessionStorage.getItem('token');
 const url = 'https://cleave-automart.herokuapp.com/api/v1';
 
+/**
+ * ADS TEMPLATE TO DISPLAY ON PAGE
+ * @param {object} data
+ */
 const displayMyAds = async (data) => {
   const template = `<div id="ads-box" car-id=${data.car_id}>
     <img src=${data.image_url}>
@@ -18,22 +20,35 @@ const displayMyAds = async (data) => {
     <div class="update-container">
         <button status=${data.status} class="sold-botton">SOLD</button>
         <input name= "price" type="number" placeholder="price">
-        <button class=update-button>Update</button>
+        <button status=${data.status} class=update-button>Update</button>
     </div>
     </div>`;
   await display.insertAdjacentHTML('afterbegin', template);
 };
 
-
+/** DISABLE BUTTONS WHEN CAR IS SOLD */
 window.addEventListener('mouseover', (e) => {
   if (e.target.className === 'sold-botton') {
     if (e.target.getAttribute('status') === 'sold') {
+      e.target.parentElement.childNodes[3].disabled = true;
       e.target.disabled = true;
       e.target.className = 'sold';
+      return false;
+    }
+  }
+  if (e.target.className === 'update-button') {
+    if (e.target.getAttribute('status') === 'sold') {
+      e.target.disabled = true;
+      e.target.parentElement.childNodes[3].disabled = true;
+      e.target.className = 'sold';
+      return false;
     }
   }
 });
 
+/**
+ * LOAD USER POSTED ADS
+ */
 const getMyAds = async () => {
   fetch(`${url}/owner/car`, {
     method: 'GET',
@@ -56,6 +71,10 @@ const getMyAds = async () => {
 };
 getMyAds();
 
+/**
+ * UPDATE CAR STATUS
+ * @param {params} id
+ */
 const carSold = async (id) => {
   fetch(`${url}/car/${id}/status`, {
     method: 'PATCH',
@@ -75,14 +94,47 @@ const carSold = async (id) => {
     });
 };
 
+/**
+ *UPDATE CAR PRICE
+ * @param {params} id
+ * @param {number} price
+ */
+const updatePrice = (id, price) => {
+  fetch(`${url}/car/${id}/price`, {
+    method: 'PATCH',
+    headers: {
+      'x-access-token': `${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      price,
+    }),
+  })
+    .then(res => res.json())
+    .then((cars) => {
+      if (cars.error) {
+        spinner.style.display = 'none';
+        alert(cars.error);
+        return;
+      }
+      spinner.style.display = 'none';
+      location.reload();
+    });
+};
+
+/** ADD EVENT LISTENERS TO BUTTONS */
 document.addEventListener('click', (e) => {
   const id = e.target.parentElement.parentElement.getAttribute('car-id');
+  const carprice = Number(e.target.parentElement.childNodes[3].value);
   if (e.target.className === 'sold-botton') {
-    console.log(e.target);
     const sure = confirm('Please confirm');
     if (sure === true) {
       carSold(id);
-      console.log(id);
+    }
+  } else if (e.target.className === 'update-button') {
+    const sure = confirm('Please confirm');
+    if (sure === true) {
+      updatePrice(id, carprice);
     }
   }
 });
