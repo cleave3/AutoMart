ed490bfc664c29ca1e5c7975b38c406bde5ad044
cleave3 <owhiroroeghele@gmail.com>
2019-07-11 20:22:00 +0100

    Refactor orders endpoint to suit endpoint specification

--- server/database/migrations/migrations.js
+++ server/database/migrations/migrations.js
@@ -41,10 +41,7 @@ const createTables = () => {
     created_on TIMESTAMP DEFAULT NOW(),
     amount FLOAT NOT NULL,
     price_offered FLOAT NOT NULL,
-    status TEXT NOT NULL,
-    manufacturer TEXT NOT NULL,
-    model VARCHAR NOT NULL,
-    image_url VARCHAR NOT NULL
+    status TEXT NOT NULL
   );
   `;
   pool.query(tables)
